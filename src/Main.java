7a8ea837274a9308c20f683f1d551462cd6db1e2
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2017-04-19 15:06:53 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Main {
+
+    static int trials = 1;
+
+    public static void main(String[] args) {
+
+        tryManyTimes(65261, false, 1, 65261);
+        /*
+        Object[] primes = new Object[]{
+                "899809343",
+                "20988936657440586486151264256610222593863921",
+                mersenne(19),
+                mersenne(31),
+                mersenne(61),
+                mersenne(89),
+                mersenne(107),
+                mersenne(127),
+                mersenne(521),
+                mersenne(607)
+        };
+
+        for (int m = 1; m < 5; m++) {
+            testAll(primes, m);
+            testFile("primes.txt", m);
+        }
+
+        for (int m = 1; m < 5; m++) {
+            for (Object p : primes) {
+                tryManyTimes(p, true, m,5000);
+                tryManyTimes(getNum(p).add(BigInteger.valueOf(6)), false, m,1000);
+            }
+        }
+        */
+
+
+    }
+
+    public static BigInteger getNum(Object num) {
+        BigInteger n;
+        if (num instanceof String)
+            n = new BigInteger((String) num);
+        else if (num instanceof BigInteger)
+            n = (BigInteger) num;
+        else if (num instanceof BigDecimal)
+            n = ((BigDecimal) num).toBigInteger();
+        else if (num instanceof Number)
+            n = BigInteger.valueOf(((Number) num).longValue());
+        else
+            throw new NumberFormatException();
+
+        return n;
+    }
+
+    public static void testAll(Object[] nums, int m) {
+        BigInteger n;
+        int wrong = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            n = getNum(nums[i]);
+            if (!PrimalityTester.isPrime(n, m)) {
+                System.err.println(i+ ") Error: " + n + " is found to be not prime");
+                wrong++;
+            }
+            wrong = PrimalityTester.isPrime(n, m) ? 0 : 1;
+        }
+
+        printStats(nums.length, m, wrong);
+    }
+
+    public static void testFile(String fileName, int m) {
+
+        int wrong = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            BigInteger i = BigInteger.ZERO;
+
+            while ((line = br.readLine()) != null) {
+
+                BigInteger nextPrime = new BigInteger(line);
+
+
+                for(i = nextPrime.add(BigInteger.ONE); i.compareTo(nextPrime) < 0; i = i.add(BigInteger.ONE)) {
+                    if (PrimalityTester.isPrime(i, m)) {
+                        System.out.println(i + " is found to be prime when it isn't.");
+                        wrong++;
+                    }
+                }
+
+                if (!PrimalityTester.isPrime(nextPrime, m)) {
+                    System.out.println(i + " is found to be not prime when it is.");
+                    wrong++;
+                }
+
+            }
+
+            printStats(i.intValue(), m, wrong);
+
+        } catch (Exception e) {
+            System.out.println("Cannot open file");
+        }
+    }
+
+    public static void printStats(int total, int m, int wrong) {
+        if (total == 0) return;
+
+        if (wrong != 0)
+            System.out.println("<<<<<<----------------------------------------");
+
+        System.out.println("Run " + trials++);
+        System.out.println("Errors: " + wrong + "/" + total);
+        System.out.printf("Certainty (m): " + m + "\nExpected accuracy: %.2f%%\n", 100*(1 - Math.pow(2, -m)));
+        System.out.printf("Accuracy: %.2f%%\n", (total - wrong)*100.0/total);
+
+        System.out.println();
+    }
+
+    public static void tryManyTimes(Object num, boolean shouldBePrime, int m, int times) {
+        int wrong = 0;
+        for (int i = 0; i < times; i++) {
+            wrong += PrimalityTester.isPrime(getNum(num), m) == shouldBePrime ? 0 : 1;
+        }
+        printStats(times, m, wrong);
+    }
+
+
+    public static BigInteger mersenne(int m) {
+        return BigInteger.valueOf(2).pow(m).subtract(BigInteger.ONE);
+    }
+
+    public static void prova(BigInteger x) {
+
+        System.out.println("Numero: " + x);
+        System.out.println(PrimalityTester.isPrime(x, 10));
+    }
+}
---
+++ src/PrimalityTester.java
@@ -0,0 +1,106 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+/**
+ * @author afusco
+ * @date 18/04/17
+ */
+public class PrimalityTester {
+
+    private static Random r = new Random(System.currentTimeMillis());
+
+    public static boolean isPrime(BigInteger prime, double probability) {
+        if (probability <= 0.5) {
+            return isPrime(prime, 1);
+        }
+        return isPrime(prime, (int) Math.floor(Math.log(probability) / Math.log(2)));
+    }
+
+    public static boolean isPrime(BigInteger prime, int m) {
+
+        if (prime.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+            return false;
+        }
+
+        if (prime.compareTo(BigInteger.ONE) <= 0
+                || prime.mod(BigInteger.valueOf(2)).compareTo(BigInteger.ZERO) == 0) {
+            return false;
+        }
+
+        // Do not calculate every time
+        BigInteger nm1 = prime.subtract(BigInteger.ONE);
+
+        BigInteger[] b = new BigInteger[m];
+        //TODO improve random generation to make unique
+        for (int i = 0; i < m; i++) {
+            b[i] = randomBigInt(BigInteger.valueOf(2), prime);
+        }
+
+
+        for (int i = 0; i < m; i++) {
+
+            // Fermat test
+            if (b[i].modPow(prime.subtract(BigInteger.ONE), prime).compareTo(BigInteger.ONE) != 0) {
+                return false;
+            }
+
+
+            BigInteger twoToJ = BigInteger.valueOf(2);
+
+            // Start from biggest j so k is minimum
+            // No logarithm operation is available with BigIntegers, so this will take log2(n) iterations to find j
+            //while (twoToJ.compareTo(nm1) <= 0) { twoToJ = twoToJ.multiply(BigInteger.valueOf(2)); }
+
+            //BitLength of a BigInteger is ceil(log_2(x))
+            twoToJ = BigInteger.valueOf(2).pow(nm1.bitLength());
+
+            while (twoToJ.compareTo(BigInteger.ONE) > 0) {
+
+                // k[0] = n-1 / (2^j)
+                // k[1] = n-1 % (2^j)
+                BigInteger[] k = nm1.divideAndRemainder(twoToJ);
+
+                if (k[1].compareTo(BigInteger.ZERO) == 0) {
+
+                    // modPow can be used because gcd(a,b) = gcd(b,a%b) (euclid's algorithm)
+                    // also, the subtraction can be performed later, because
+                    // (b^k-1) % nint = ((b^k) % nint) -1
+                    BigInteger gcd = b[i].modPow(k[0], prime).subtract(BigInteger.ONE).mod(prime).gcd(prime);
+
+                    if (gcd.compareTo(prime) < 0 && gcd.compareTo(BigInteger.ONE) > 0) {
+                        return false;
+                    }
+                }
+
+                twoToJ = twoToJ.divide(BigInteger.valueOf(2));
+            }
+
+            //System.out.println("\n\n");
+        }
+
+        return true;
+    }
+
+
+    private static BigInteger randomBigInt(BigInteger max) {
+        BigInteger i;
+        do {
+            i = new BigInteger(max.bitLength(), r);
+        } while (i.compareTo(max) >= 0);
+        return i;
+    }
+
+    private static BigInteger randomBigInt(BigInteger min, BigInteger max) {
+        return randomBigInt(max.subtract(min)).add(min);
+    }
+
+    public static BigInteger gcd(BigInteger p, BigInteger q) {
+        while (q.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger temp = q;
+            q = p.mod(q);
+            p = temp;
+        }
+        return p;
+    }
+
+}
